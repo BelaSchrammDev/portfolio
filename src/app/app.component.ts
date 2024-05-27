66cb8c2b1e517b887662eef46d747acb5a69749f
BelaSchrammDev <belaschramm@aol.de>
2024-05-27 23:36:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    LandingpageComponent,
+    AboutmeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
