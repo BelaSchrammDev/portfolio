8d081db229c19893a7f9865a76630c1323aa3f9e
BelaSchrammDev <belaschramm@aol.de>
2024-06-06 18:44:12 +0200

    animate the arrow

--- src/app/arrowspace/arrowspace.component.ts
+++ src/app/arrowspace/arrowspace.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-arrowspace',
@@ -9,5 +9,31 @@ import { Component, Input } from '@angular/core';
   styleUrl: './arrowspace.component.scss'
 })
 export class ArrowspaceComponent {
+
+  animationPlayed: boolean = false;
+
   @Input() alignment: string = 'left';
+  @Input() section: string = `section_${Math.random().toString(36).substr(2, 9)}`;
+
+  @HostListener('mouseover', ['$event'])
+
+  onMouseOver(event: MouseEvent) {
+    if (this.animationPlayed) return;
+    const animElement = document.getElementById(this.section) as unknown as SVGAnimateElement;
+    if (animElement) {
+      animElement.beginElement();
+      this.animationPlayed = true;
+    }
+  }
+
+  @HostListener('mouseleave', ['$event'])
+
+  onMouseLeave(event: MouseEvent) {
+    if (!this.animationPlayed) return;
+    const animElement = document.getElementById(this.section + 'return') as unknown as SVGAnimateElement;
+    if (animElement) {
+      animElement.beginElement();
+      this.animationPlayed = false;
+    }
+  }
 }
