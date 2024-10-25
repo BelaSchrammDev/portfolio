3470d189eac59d7ea7cf624b36559cfde78bb25a
BelaSchrammDev <belaschramm@aol.de>
2024-10-25 19:51:51 +0200

    add c# skill icon

--- src/app/mainpage/myskills/myskills.component.ts
+++ src/app/mainpage/myskills/myskills.component.ts
@@ -12,15 +12,16 @@ export class MyskillsComponent {
   translate = inject(MytranslateService);
 
   skillSet = [
-    { name: 'Angular', icon: 'angular' },
-    { name: 'API', icon: 'api' },
-    { name: 'CSS', icon: 'css' },
-    { name: 'Firebase', icon: 'firebase' },
-    { name: 'Git', icon: 'git' },
-    { name: 'HTML', icon: 'html' },
-    { name: 'JavaScript', icon: 'javascript' },
-    { name: 'TypeScript', icon: 'typescript' },
-    { name: 'Scrum', icon: 'scrum' },
-    { name: 'Material Design', icon: 'material' },
+    { name: 'Angular', icon: 'angular.svg' },
+    { name: 'API', icon: 'api.svg' },
+    { name: 'CSS', icon: 'css.svg' },
+    { name: 'Firebase', icon: 'firebase.svg' },
+    { name: 'Git', icon: 'git.svg' },
+    { name: 'HTML', icon: 'html.svg' },
+    { name: 'JavaScript', icon: 'javascript.svg' },
+    { name: 'TypeScript', icon: 'typescript.svg' },
+    { name: 'Scrum', icon: 'scrum.svg' },
+    { name: 'Material Design', icon: 'material.svg' },
+    { name: 'C#', icon: 'csharp.png' },
   ]
 }
