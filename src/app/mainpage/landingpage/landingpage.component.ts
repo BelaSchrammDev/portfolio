c43bc0c60c051b9e51fe3458cc1706ae398e2649
BelaSchrammDev <belaschramm@aol.de>
2024-12-04 19:23:54 +0100

    add welcome audio to landingpage

--- src/app/mainpage/landingpage/landingpage.component.ts
+++ src/app/mainpage/landingpage/landingpage.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { MytranslateService } from '../../shared/services/mytranslate.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
 export class LandingpageComponent {
+  showAudioPlayer = false;
+  playingAudioPlayer = false;
   translate = inject(MytranslateService);
 }
