76dfb8dc5b317448db6d3cb4cf2932fb2d753224
Bela Schramm <belaschramm@aol.de>
2026-09-19 12:40:34 +0200

    add privacy.en

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PrivacydeComponent } from './privacyde/privacyde.component';
 import { PrivacyenComponent } from './privacyen/privacyen.component';
+import { MytranslateService } from '../shared/services/mytranslate.service';
 
 @Component({
   selector: 'app-privacy',
@@ -13,6 +14,7 @@ import { PrivacyenComponent } from './privacyen/privacyen.component';
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent implements OnInit {
+  translate = inject(MytranslateService)
 
   ngOnInit() {
     window.scrollTo({
